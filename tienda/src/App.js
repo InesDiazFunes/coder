c129124afc1d28152f84bfaad098c55a9d46593b
Ines Diaz Funes <inesdiazfunes123@gmail.com>
2023-03-25 14:36:46 -0300

    -

--- tienda/src/App.js
+++ tienda/src/App.js
@@ -1,12 +1,16 @@
 import './style.css';
 import NavBar from './components/header/NavBar';
 import ContainerCardItems from './components/component item/ContainerCardItems';
+import { BrowserRouter , Route , Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
-    <NavBar></NavBar>
-    <ContainerCardItems></ContainerCardItems>
-    </>
+    <BrowserRouter>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/' element={<ContainerCardItems/>}/>
+      </Routes>
+    </BrowserRouter>
     );
 }
 
